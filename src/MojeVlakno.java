3d530344d2c85f30af0592c09da067e4e9a39c12
zikl <tadeas.zikl@gmail.com>
2024-05-13 08:00:32 +0200

    add int j

--- src/MojeVlakno.java
+++ src/MojeVlakno.java
@@ -2,6 +2,7 @@ public class MojeVlakno implements Runnable{
     public void run() {
         for (int i = 65; i < 106; i++){
             System.out.println((char)i + " ");
+            int j = 0;
         }
 
     }
